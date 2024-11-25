8f7f97bd08e8a51911c6fa708789fe96ba494c5f
Lekuru <contact@lekuru.xyz>
2024-11-25 17:08:34 +0100

    Apply 100 mb limit on startup

--- osz2-service/Program.cs
+++ osz2-service/Program.cs
@@ -11,5 +11,16 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
+// Setup a ~100mb limit for file uploads
+app.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 104857600;
+});
+app.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Limits.MaxRequestBodySize = 104857600;
+});
+
+
 app.UseRouting().UseEndpoints(endpoints => endpoints.MapControllers());
 app.Run();
